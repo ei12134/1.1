a97e819cb3cf8b40aefd24fbde73a7442d2fee7f
zukuyama <ei12135@fe.up.pt>
2014-03-01 16:46:37 +0000

    ja compila mas ainda n esta bem

--- src/Labyrinth.java
+++ src/Labyrinth.java
@@ -2,6 +2,11 @@ import java.util.ArrayList;
 import java.util.Random;
 import java.util.Scanner;
 
+import com.sun.org.apache.bcel.internal.generic.GOTO;
+import com.sun.org.apache.bcel.internal.generic.GotoInstruction;
+
+import sun.java2d.loops.FillRect.General;
+
 public class Labyrinth {
 	private int size;
 	private Hero hero;
@@ -11,6 +16,231 @@ public class Labyrinth {
 	public Labyrinth() {
 
 	};
+
+	public void RandomLabyringth( ) {
+		// boolean[][] visited;
+
+		labyrinth = new ArrayList<ArrayList<Position>>();
+		size = 11; // gotta be odd with this metod, later on will b an user
+					// choise(aslong it is odd)
+		// visited = new boolean[size + 2][size + 2];
+		// // fill with spaces
+		for (int i = 0; i < size; i++) {
+			ArrayList<Position> labyrinthLine = new ArrayList<Position>();
+			for (int z = 0; z < size; z++)
+				labyrinthLine.add(new Position(i, z, Piece.wallChar));
+			labyrinth.add(labyrinthLine);
+		}
+
+		// start position
+		Random ry = new Random();
+		// int randomNumline = ry.nextInt(size);//substituir por size later on
+		// tem de ser odd
+		Random rx = new Random();
+		// int randomNumcolum = rx.nextInt(size);//substituir por size later on,
+		// tem de ser odd
+		int randomNumline = 5;
+		int randomNumcolum = 3;
+		// while (randomNumline%2 ==0){
+		// randomNumline = ry.nextInt(size);
+		// }
+		// while (randomNumcolum%2 ==0){
+		// randomNumcolum = ry.nextInt(size);
+		// }
+
+		labyrinth.get(randomNumline).get(randomNumcolum).SetId(Piece.emptyChar);
+		// visited[randomNumline][randomNumcolum] = true; // marca o ponto
+		// inicial
+		// como visitado
+
+		// boolean completed = false;
+		// boolean completedr = false;
+		// boolean completedl = false;
+		// boolean completedu = false;
+		// boolean comp,letedd = false;
+		int numberiterations =4;
+do{
+		Random r = new Random();
+		int randomNum = r.nextInt(4);
+
+		if ((labyrinth.get(randomNumline + 2).get(randomNumcolum).GetId() == Piece.wallChar)
+				|| (labyrinth.get(randomNumline - 2).get(randomNumcolum)
+						.GetId() == Piece.wallChar)
+				|| (labyrinth.get(randomNumcolum + 2).get(randomNumcolum)
+						.GetId() == Piece.wallChar)
+				|| (labyrinth.get(randomNumcolum - 2).get(randomNumcolum)
+						.GetId() == Piece.wallChar)) {
+			
+			switch (randomNum) {
+			case 0:
+
+				if ((randomNumline + 2 < size)
+						&& (labyrinth.get(randomNumline + 2)
+								.get(randomNumcolum).GetId() == Piece.wallChar)) {
+					labyrinth.get(randomNumline + 1).get(randomNumcolum)
+							.SetId(Piece.emptyChar);
+					labyrinth.get(randomNumline + 2).get(randomNumcolum)
+							.SetId(Piece.emptyChar);
+					numberiterations--;
+				}
+				break;
+				
+			case 1:
+
+				if ((randomNumline - 2 >= 0)
+						&& (labyrinth.get(randomNumline - 2)
+								.get(randomNumcolum).GetId() == Piece.wallChar)) {
+					labyrinth.get(randomNumline - 1).get(randomNumcolum)
+							.SetId(Piece.emptyChar);
+					labyrinth.get(randomNumline - 2).get(randomNumcolum)
+							.SetId(Piece.emptyChar);
+					numberiterations--;
+
+				}
+				break;
+			case 2:
+
+				if ((randomNumcolum + 2 < size)
+						&& (labyrinth.get(randomNumline)
+								.get(randomNumcolum + 2).GetId() == Piece.wallChar)) {
+					labyrinth.get(randomNumline).get(randomNumcolum + 1)
+							.SetId(Piece.emptyChar);
+					labyrinth.get(randomNumline).get(randomNumcolum + 2)
+							.SetId(Piece.emptyChar);
+					numberiterations--;
+
+				}
+				break;
+			case 3:
+
+				if ((randomNumcolum - 2 >= 0)
+						&& (labyrinth.get(randomNumline)).get(
+								randomNumcolum - 2).GetId() == Piece.wallChar) {
+					labyrinth.get(randomNumline).get(randomNumcolum - 1)
+							.SetId(Piece.emptyChar);
+					labyrinth.get(randomNumline).get(randomNumcolum - 2)
+							.SetId(Piece.emptyChar);
+					numberiterations--;
+
+				}
+				break;
+			default:
+				break;
+			}
+		} else {
+
+			switch (randomNum) {
+			case 0:
+
+				if ((randomNumline + 2 < size)
+						&& (labyrinth.get(randomNumline + 2)
+								.get(randomNumcolum).GetId() == Piece.emptyChar)) {
+					randomNumline += 2;
+					numberiterations--;
+				}
+
+				break;
+			case 1:
+
+				if ((randomNumline - 2 >= 0)
+						&& (labyrinth.get(randomNumline - 2)
+								.get(randomNumcolum).GetId() == Piece.emptyChar)) {
+					randomNumline -= 2;
+					numberiterations--;
+
+				}
+				break;
+			case 2:
+
+				if ((randomNumcolum + 2 < size)
+						&& (labyrinth.get(randomNumline)
+								.get(randomNumcolum + 2).GetId() == Piece.emptyChar)) {
+					randomNumcolum += 2;
+					numberiterations--;
+
+				}
+				break;
+			case 3:
+
+				if ((randomNumcolum - 2 >= 0)
+						&& (labyrinth.get(randomNumline)).get(
+								randomNumcolum - 2).GetId() == Piece.emptyChar) {
+					randomNumcolum -= 2;
+					numberiterations--;
+
+				}
+				break;
+			default:
+				break;
+			}
+		}
+}while( numberiterations > 0);
+		// for (int i = 0; i < size; i++) {
+		// // upper and lower labyrinth border limits
+		// labyrinth.get(i).get(0).SetId(Piece.wallChar);
+		// labyrinth.get(i).get(size - 1).SetId(Piece.wallChar);
+		//
+		// // left and right labyrinth border limits
+		// labyrinth.get(0).get(i).SetId(Piece.wallChar);
+		// labyrinth.get(size - 1).get(i).SetId(Piece.wallChar);
+		// }
+		// //
+		// labyrinth.get(2).get(2).SetId(Piece.wallChar);
+		// labyrinth.get(2).get(3).SetId(Piece.wallChar);
+		// labyrinth.get(2).get(4).SetId(Piece.wallChar);
+		// labyrinth.get(2).get(6).SetId(Piece.wallChar);
+		// labyrinth.get(2).get(7).SetId(Piece.wallChar);
+		// labyrinth.get(2).get(8).SetId(Piece.wallChar);
+		// //
+		// labyrinth.get(3).get(2).SetId(Piece.wallChar);
+		// labyrinth.get(3).get(3).SetId(Piece.wallChar);
+		// labyrinth.get(3).get(4).SetId(Piece.wallChar);
+		// labyrinth.get(3).get(6).SetId(Piece.wallChar);
+		// labyrinth.get(3).get(7).SetId(Piece.wallChar);
+		// labyrinth.get(3).get(8).SetId(Piece.wallChar);
+		// //
+		// labyrinth.get(5).get(2).SetId(Piece.wallChar);
+		// labyrinth.get(5).get(3).SetId(Piece.wallChar);
+		// labyrinth.get(5).get(4).SetId(Piece.wallChar);
+		// labyrinth.get(5).get(6).SetId(Piece.wallChar);
+		// labyrinth.get(5).get(7).SetId(Piece.wallChar);
+		// //
+		// labyrinth.get(7).get(2).SetId(Piece.wallChar);
+		// labyrinth.get(7).get(3).SetId(Piece.wallChar);
+		// labyrinth.get(7).get(4).SetId(Piece.wallChar);
+		// labyrinth.get(7).get(5).SetId(Piece.wallChar);
+		// labyrinth.get(7).get(6).SetId(Piece.wallChar);
+		// labyrinth.get(7).get(7).SetId(Piece.wallChar);
+
+		// // hero position
+		// hero = new Hero(labyrinth.get(1).get(1), Status.alive);
+		// labyrinth.get(hero.GetPosition().GetX()).get(hero.GetPosition().GetY())
+		// .SetId(Piece.heroChar);
+
+		// labyrinth.get(hero.GetPosition().GetX()).get(hero.GetPosition().GetY()).SetId(Piece.emptyChar);
+		// hero.SetPosition(labyrinth.get(1).get(2));
+		// labyrinth.get(hero.GetPosition().GetX()).get(hero.GetPosition().GetY()).SetId(Piece.heroChar);
+
+		// dragon position
+		// dragon = new Dragon(labyrinth.get(1).get(3), Status.alive,
+		// Piece.dragonChar);
+		// labyrinth.get(dragon.GetPosition().GetX())
+		// .get(dragon.GetPosition().GetY()).SetId(Piece.dragonChar);
+
+		// exit position
+		// labyrinth.get(9).get(5).SetId(Piece.exitChar);
+
+		// sword position
+		// labyrinth.get(1).get(8).SetId(Piece.swordChar);
+
+		// starts the game
+
+	
+
+	
+		DisplayLabyrinth();
+		// HeroMove();
+	}
 
 	public void StandardLabyrinth() {
 
@@ -243,29 +473,37 @@ public class Labyrinth {
 					.GetStatus() == Status.asleep)
 					&& (hero.GetStatus() != Status.cleared)) {
 				if ((labyrinth.get(hero.GetPosition().GetX() + 1).get(
-						hero.GetPosition().GetY()) == labyrinth.get(dragon.GetPosition().GetX()).get(
+						hero.GetPosition().GetY()) == labyrinth.get(
+						dragon.GetPosition().GetX()).get(
+						dragon.GetPosition().GetY()))
+						|| (labyrinth.get(hero.GetPosition().GetX() - 1).get(
+								hero.GetPosition().GetY()) == labyrinth.get(
+								dragon.GetPosition().GetX()).get(
 								dragon.GetPosition().GetY()))
-						|| (labyrinth.get(hero.GetPosition().GetX() - 1).get(
-								hero.GetPosition().GetY()) == labyrinth.get(dragon.GetPosition().GetX()).get(
+						|| (labyrinth.get(hero.GetPosition().GetX()).get(
+								hero.GetPosition().GetY() + 1) == labyrinth
+								.get(dragon.GetPosition().GetX()).get(
 										dragon.GetPosition().GetY()))
 						|| (labyrinth.get(hero.GetPosition().GetX()).get(
-								hero.GetPosition().GetY() + 1) == labyrinth.get(dragon.GetPosition().GetX()).get(
-										dragon.GetPosition().GetY()))
-						|| (labyrinth.get(hero.GetPosition().GetX()).get(
-								hero.GetPosition().GetY() - 1) == labyrinth.get(dragon.GetPosition().GetX()).get(
+								hero.GetPosition().GetY() - 1) == labyrinth
+								.get(dragon.GetPosition().GetX()).get(
 										dragon.GetPosition().GetY()))
 						|| (labyrinth.get(hero.GetPosition().GetX() - 1).get(
-								hero.GetPosition().GetY() + 1) == labyrinth.get(dragon.GetPosition().GetX()).get(
+								hero.GetPosition().GetY() + 1) == labyrinth
+								.get(dragon.GetPosition().GetX()).get(
 										dragon.GetPosition().GetY()))
 						|| (labyrinth.get(hero.GetPosition().GetX() + 1).get(
-								hero.GetPosition().GetY() + 1) == labyrinth.get(dragon.GetPosition().GetX()).get(
+								hero.GetPosition().GetY() + 1) == labyrinth
+								.get(dragon.GetPosition().GetX()).get(
 										dragon.GetPosition().GetY()))
 						|| (labyrinth.get(hero.GetPosition().GetX() + 1).get(
-								hero.GetPosition().GetY() - 1) == labyrinth.get(dragon.GetPosition().GetX()).get(
+								hero.GetPosition().GetY() - 1) == labyrinth
+								.get(dragon.GetPosition().GetX()).get(
 										dragon.GetPosition().GetY()))
 						|| (labyrinth.get(hero.GetPosition().GetX() - 1).get(
-								hero.GetPosition().GetY() - 1) == labyrinth.get(dragon.GetPosition().GetX()).get(
-										dragon.GetPosition().GetY()))){
+								hero.GetPosition().GetY() - 1) == labyrinth
+								.get(dragon.GetPosition().GetX()).get(
+										dragon.GetPosition().GetY()))) {
 					if (hero.GetStatus() == Status.armed) {
 						dragon.SetStatus(Status.dead);
 						labyrinth.get(dragon.GetPosition().GetX())
@@ -282,7 +520,7 @@ public class Labyrinth {
 					}
 				}
 			}
-			if (dragon.GetStatus() != Status.dead )
+			if (dragon.GetStatus() != Status.dead)
 				DragonMove(5);
 			DisplayLabyrinth();
 
@@ -338,7 +576,7 @@ public class Labyrinth {
 		case 2:
 			if (labyrinth.get(dragon.GetPosition().GetX() - 1)
 					.get(dragon.GetPosition().GetY()).GetId() != Piece.wallChar) {
-				
+
 				if (dragon.GetStatus() == Status.asleep) {
 					dragon.SetStatus(Status.alive);
 					dragon.SetPiece(Piece.dragonChar);
@@ -371,7 +609,7 @@ public class Labyrinth {
 		case 3:
 			if (labyrinth.get(dragon.GetPosition().GetX())
 					.get(dragon.GetPosition().GetY() + 1).GetId() != Piece.wallChar) {
-				
+
 				if (dragon.GetStatus() == Status.asleep) {
 					dragon.SetStatus(Status.alive);
 					dragon.SetPiece(Piece.dragonChar);
@@ -404,7 +642,7 @@ public class Labyrinth {
 		case 4:
 			if (labyrinth.get(dragon.GetPosition().GetX())
 					.get(dragon.GetPosition().GetY() - 1).GetId() != Piece.wallChar) {
-				
+
 				if (dragon.GetStatus() == Status.asleep) {
 					dragon.SetStatus(Status.alive);
 					dragon.SetPiece(Piece.dragonChar);
