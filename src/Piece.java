70a198207e10c20e522c31e0c0473242eefcfed1
unknown <ei12134@fe.up.pt>
2014-03-17 10:48:54 +0000

    remove old src

--- src/Piece.java
+++
@@ -1,7 +0,0 @@
-public class Piece {
-
-	static final String heroChar = " H ", armedChar = " A ", dragonChar = " D ",
-			asleepChar = " d ", guardingChar = " F ", exitChar = " S ",
-			swordChar = " E ", wallChar = " X ", emptyChar = "   ",
-			guardingAsleepChar = " f ", eagleChar = "G";
-}
--- src/Status.java
+++
@@ -1,6 +0,0 @@
-public class Status {
-
-	static final String alive = "alive", dead = "dead", guarding = "guarding",
-			armed = "armed", asleep = "asleep", idle = "idle",
-			cleared = "cleared", pursuing = "pursuing", returning = "returning";
-}
