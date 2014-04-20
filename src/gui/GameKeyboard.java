7eaca9000fd4709a058401adc2f69bad47fd930e
André Pinheiro <andrepdpinheiro@gmail.com>
2014-04-20 22:41:40 +0100

    A fazer a configuração de teclado

--- src/gui/GameKeyboard.java
+++ src/gui/GameKeyboard.java
@@ -1,24 +1,18 @@
 package gui;
 
-import java.awt.event.KeyEvent;
 import java.io.File;
-import java.io.FileInputStream;
-import java.io.FileOutputStream;
-import java.io.ObjectInputStream;
-import java.io.ObjectOutputStream;
-import java.util.ArrayList;
 import java.util.HashMap;
 
 public class GameKeyboard {
 
 	private HashMap<String, Integer> keys;
-	private ArrayList<KeyboardKey> listKeys;
-	String filename = "gameKeys.lpoo";
+	private String filename = "gameKeys.lpoo";
+	private KeyboardConfig keyConfig;
 
 	
 	public GameKeyboard() {
 		keys = new HashMap<String, Integer>();
-		listKeys = new ArrayList<KeyboardKey>();
+		keyConfig = new KeyboardConfig();
 	}
 
 	
@@ -27,90 +21,16 @@ public class GameKeyboard {
 		if(!f.exists()) {
 			try {
 				f.createNewFile();
-				createStandardKeys();
+				keyConfig.writeDefaultConfig();
 			} catch(Exception e) {
 				e.printStackTrace();
 			}
 		} else {
 			//Read the configuration file
+			keys = keyConfig.getKeys();
 		}
 	}
 	
-	
-	public void createStandardKeys() {
-		FileOutputStream outStream = null;
-		ObjectOutputStream objStream = null;
-		try {
-			outStream = new FileOutputStream(filename);
-			objStream = new ObjectOutputStream(outStream);
-			//Default ones don't overwrite
-			listKeys.add(new KeyboardKey("up_arrow", KeyEvent.VK_UP));
-			listKeys.add(new KeyboardKey("down_arrow", KeyEvent.VK_DOWN));
-			listKeys.add(new KeyboardKey("left_arrow", KeyEvent.VK_LEFT));
-			listKeys.add(new KeyboardKey("right_arrow", KeyEvent.VK_RIGHT));
-			listKeys.add(new KeyboardKey("eagle_spacebar", KeyEvent.VK_SPACE));
-			//objStream.writeObject(new KeyboardKey("up_arrow", KeyEvent.VK_UP));
-			//objStream.writeObject(new KeyboardKey("down_arrow",
-				//	KeyEvent.VK_DOWN));
-			//objStream.writeObject(new KeyboardKey("left_arrow",
-				//	KeyEvent.VK_LEFT));
-			//objStream.writeObject(new KeyboardKey("right_arrow",
-				//	KeyEvent.VK_RIGHT));
-			//objStream.writeObject(new KeyboardKey("eagle_spacebar",
-				//	KeyEvent.VK_SPACE));
-
-			//Allow to customize
-			listKeys.add(new KeyboardKey("up", KeyEvent.VK_W));
-			listKeys.add(new KeyboardKey("down", KeyEvent.VK_S));
-			listKeys.add(new KeyboardKey("left", KeyEvent.VK_A));
-			listKeys.add(new KeyboardKey("right", KeyEvent.VK_D));
-			listKeys.add(new KeyboardKey("eagle", KeyEvent.VK_E));
-			objStream.writeObject(listKeys);
-			//objStream.writeObject(new KeyboardKey("up", KeyEvent.VK_W));
-			//objStream.writeObject(new KeyboardKey("down", KeyEvent.VK_S));
-			//objStream.writeObject(new KeyboardKey("left", KeyEvent.VK_A));
-			//objStream.writeObject(new KeyboardKey("right", KeyEvent.VK_D));
-			//objStream.writeObject(new KeyboardKey("eagle", KeyEvent.VK_E));
-			objStream.close();
-
-		} catch (Exception e) {
-			e.printStackTrace();
-		}
-	}
-	
-	
-	public void readKeyboardKeys() {
-		
-	}
-	
-
-	public void readKeys() {
-		FileInputStream inStream = null;
-		ObjectInputStream objInStream = null;
-		try {
-			ArrayList<KeyboardKey> key = new ArrayList<KeyboardKey>();
-			inStream = new FileInputStream(filename);
-			objInStream = new ObjectInputStream(inStream);
-
-			Object o = objInStream.readObject();
-			if(o instanceof ArrayList) {
-				key = (ArrayList<KeyboardKey>) o;
-				KeyboardKey tmp;
-				for(int i = 0; i < key.size(); i++) {
-					tmp = key.get(i);
-					keys.put(tmp.getCommand(), tmp.getCommandChar());
-				}
-			}
-			/**KeyboardKey tmp;
-			for (int i = 0; i < 10; i++) {
-				tmp = (KeyboardKey) objInStream.readObject();
-				keys.put(tmp.getCommand(), tmp.getCommandChar());
-			}*/
-			objInStream.close();
-		} catch (Exception e) {
-			e.printStackTrace();
-		}
-	}
 
 	public HashMap<String, Integer> getKeys() {
 		if (keys.size() > 0)
--- src/gui/KeyboardConfiguration.java
+++ src/gui/KeyboardConfiguration.java
@@ -1,24 +1,53 @@
 package gui;
 
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.awt.event.KeyEvent;
 import java.awt.event.KeyListener;
 import java.util.HashMap;
 
+import javax.swing.JButton;
 import javax.swing.JOptionPane;
 import javax.swing.JPanel;
+import javax.swing.JTextField;
 
 public class KeyboardConfiguration extends JPanel implements KeyListener {
 
 	private static final long serialVersionUID = 1L;
-	private HashMap<String, Integer> gameKeys;
+	private HashMap<String, Integer> keys;
 	private Menu menu;
+	private JTextField up, down, left, right;
+	private JButton submitData;
 	private Settings settings;
+	private KeyboardConfig keyConfig;
+	
 
 	KeyboardConfiguration(Menu menu, Settings settings) {
 		this.menu = menu;
 		this.settings = settings;
-		gameKeys = menu.getKeyboardKeys();
+		keys = new HashMap<String, Integer>();
+		up = new JTextField();
+		down = new JTextField();
+		left = new JTextField();
+		right = new JTextField();
+		submitData = new JButton("Guardar atalhos!");
+		keyConfig = new KeyboardConfig();
+		add(up);
+		add(down);
+		add(left);
+		add(right);
+		add(submitData);
 		addKeyListener(this);
+		
+		//Read the keyboard data
+		readKeyboardData();
+		submitData.addActionListener(new ActionListener() {
+			
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				saveData();
+			}
+		});
 	}
 
 	
@@ -39,10 +68,31 @@ public class KeyboardConfiguration extends JPanel implements KeyListener {
 		}
 	}
 
+	
+	public void saveData() {
+		if(up.getText().toUpperCase() == "E" || down.getText().toUpperCase() == "E" || 
+				left.getText().toUpperCase() == "E" || right.getText().toUpperCase() == "E")
+			JOptionPane.showMessageDialog(null, "As teclas escolhidas s‹o inv‡idas!");
+		else
+			keyConfig.writeCustomConfig(up, down, left, right);
+	}
+	
+	
+	public void readKeyboardData() {
+		keys = keyConfig.getKeys();
+		
+		up.setText(String.valueOf((char)Integer.parseInt(keys.get("up").toString())));
+		down.setText(String.valueOf((char)Integer.parseInt(keys.get("down").toString())));
+		left.setText(String.valueOf((char)Integer.parseInt(keys.get("left").toString())));
+		right.setText(String.valueOf((char)Integer.parseInt(keys.get("right").toString())));
+	}
+	
+	
 	@Override
 	public void keyReleased(KeyEvent arg0) {
 	}
 
+	
 	@Override
 	public void keyTyped(KeyEvent arg0) {
 	}
